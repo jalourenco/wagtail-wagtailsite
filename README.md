e061fe6f6ec62bf60712059c7293e3f74d1e6339
Dave Cranwell <david@torchbox.com>
2014-04-15 16:46:22 +0100

    initial empty repo

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-wagtail-wagtailsite
-===================
+Wagtail.io on Wagtail
+=======================
 
-The wagtail product site, built with Wagtail
+[Wagtail](http://wagtail.io) is distributed as a Python package, to be incorporated into a Django project via the INSTALLED_APPS setting. To get you up and running quickly, we provide a demo site with all the configuration in place, including a set of example page types.
+
+This project is essentially a clone of [wagtaildemo](http://github.com/torchbox/wagtaildemo), customised for Wagtail's own product site.
+
+Setup (with Vagrant)
+-----
+
+We recommend running Wagtail in a virtual machine using Vagrant, as this ensures that the correct dependencies are in place regardless of how your host machine is set up.
+
+### Dependencies
+* [VirtualBox](https://www.virtualbox.org/)
+* [Vagrant 1.1+](http://www.vagrantup.com)
+
+### Installation
+Run the following commands:
+
+    git clone https://github.com/torchbox/wagtail-wagtailsite.git
+    cd wagtail-wagtailsite
+    vagrant up
+    vagrant ssh
+      (then, within the SSH session:)
+    ./manage.py createsuperuser
+    ./manage.py runserver 0.0.0.0:8000
+
+This will make the app accessible on the host machine as http://localhost:8111/ - you can access the Wagtail admin interface at http://localhost:8111/admin/ . The codebase is located on the host
+machine, exported to the VM as a shared folder; code editing and Git operations will generally be done on the host.
---
+++ fabfile.py
@@ -0,0 +1,161 @@
+from __future__ import with_statement
+from fabric.api import *
+
+import uuid
+
+env.roledefs = {
+    'staging': ['tbxwagtail@django-staging.torchbox.com'],
+    'production': ['tbxwagtail@by-web-2.torchbox.com'],
+}
+
+# TODO: a nicer dict of settings
+# DATABASES = {
+#     'staging':{
+#         'name': 'tbxwagtail',
+#         'user': 'tbxwagtail',
+#     }
+#     'production':{
+#         'host': 'by-postgres-a.torchbox.com',
+#         'name': 'wagtail-torchbox',
+#         'user': 'tbxwagtail',
+#     }
+# }
+
+PROJECT = "wagtail-torchbox"
+STAGING_DB_USERNAME = "tbxwagtail"
+STAGING_DB_SERVER = "localhost"
+LIVE_DB_USERNAME = "tbxwagtail"
+LIVE_DB_SERVER = "by-postgres-a"
+DB_NAME = "wagtail-torchbox"
+LOCAL_DUMP_PATH = "~/"
+REMOTE_DUMP_PATH = "~/"
+
+
+@roles('staging')
+def deploy_staging():
+    with cd('/usr/local/django/tbxwagtail/'):
+        run("git pull")
+        run("git submodule update")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/pip install -r requirements/production.txt")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py syncdb --settings=tbx.settings.production --noinput")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py migrate --settings=tbx.settings.production --noinput")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py collectstatic --settings=tbx.settings.production --noinput")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py compress --force --settings=tbx.settings.production")
+
+    run("sudo /usr/bin/supervisorctl restart tbxwagtail")
+
+@roles('production')
+def deploy():
+    with cd('/usr/local/django/tbxwagtail/'):
+        run("git pull")
+        run("git submodule update")
+
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/pip install -r requirements/production.txt")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py syncdb --settings=tbx.settings.production --noinput")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py migrate --settings=tbx.settings.production --noinput")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py collectstatic --settings=tbx.settings.production --noinput")
+        run("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py compress --settings=tbx.settings.production")
+
+    sudo("/usr/bin/supervisorctl restart tbxwagtail")
+    #sudo("/usr/local/django/virtualenvs/tbxwagtail/bin/python manage.py update_index --settings=tbx.settings.production")
+
+# @roles('production')
+# def pull_live_data():
+#     filename = "%s-%s.sql" % (DB_NAME, uuid.uuid4())
+#     local_path = "%s%s" % (LOCAL_DUMP_PATH, filename)
+#     remote_path = "%s%s" % (REMOTE_DUMP_PATH, filename)
+#     local_db_backup_path = "%svagrant-%s-%s.sql" % (LOCAL_DUMP_PATH, DB_NAME, uuid.uuid4())
+
+#     run('pg_dump -U%s -h %s -xOf %s' % (LIVE_DB_USERNAME, LIVE_DB_SERVER, remote_path))
+#     run('gzip %s' % remote_path)
+#     get("%s.gz" % remote_path, "%s.gz" % local_path)
+#     run('rm %s.gz' % remote_path)
+    
+#     local('pg_dump -Upostgres -xOf %s %s' % (local_db_backup_path, DB_NAME))
+#     puts('Previous local database backed up to %s' % local_db_backup_path)
+    
+#     local('dropdb -Upostgres %s' % DB_NAME)
+#     local('createdb -Upostgres %s' % DB_NAME)
+#     local('gunzip %s.gz' % local_path)
+#     local('psql -Upostgres %s -f %s' % (DB_NAME, local_path))
+#     local ('rm %s' % local_path)
+
+@roles('staging')
+def pull_staging_data():
+    filename = "%s-%s.sql" % (DB_NAME, uuid.uuid4())
+    local_path = "%s%s" % (LOCAL_DUMP_PATH, filename)
+    remote_path = "%s%s" % (REMOTE_DUMP_PATH, filename)
+    local_db_backup_path = "%svagrant-%s-%s.sql" % (LOCAL_DUMP_PATH, DB_NAME, uuid.uuid4())
+
+    run('pg_dump -U%s -h %s -xOf %s' % (STAGING_DB_USERNAME, STAGING_DB_SERVER, remote_path))
+    run('gzip %s' % remote_path)
+    get("%s.gz" % remote_path, "%s.gz" % local_path)
+    run('rm %s.gz' % remote_path)
+    
+    local('pg_dump -Upostgres -xOf %s %s' % (local_db_backup_path, DB_NAME))
+    puts('Previous local database backed up to %s' % local_db_backup_path)
+    
+    local('dropdb -Upostgres %s' % DB_NAME)
+    local('createdb -Upostgres %s' % DB_NAME)
+    local('gunzip %s.gz' % local_path)
+    local('psql -Upostgres %s -f %s' % (DB_NAME, local_path))
+    local ('rm %s' % local_path)
+
+
+@roles('staging')
+def push_staging_media():
+    media_filename = "%s-%s-media.tar" % (PROJECT, uuid.uuid4())
+    local_media_dump = "%s%s" % (LOCAL_DUMP_PATH, media_filename)
+    remote_media_dump = "%s%s" % (REMOTE_DUMP_PATH, media_filename)
+
+    # tar and upload media
+    local('tar -cvf %s media' % local_media_dump)
+    local('gzip %s' % local_media_dump)
+    put('%s.gz' % local_media_dump, '%s.gz' % remote_media_dump)
+
+    # unzip everything
+    with cd('/usr/local/django/tbxwagtail/'):
+        run('rm -rf media')
+        run('mv %s.gz .' % remote_media_dump)
+        run('tar -xzvf %s.gz' % media_filename)
+        run('rm %s.gz' % media_filename)
+
+@roles('staging')
+def pull_staging_media():
+    media_filename = "%s-%s-media.tar" % (PROJECT, uuid.uuid4())
+    local_media_dump = "%s%s" % (LOCAL_DUMP_PATH, media_filename)
+    remote_media_dump = "%s%s" % (REMOTE_DUMP_PATH, media_filename)
+
+    # tar and download media
+    with cd('/usr/local/django/tbxwagtail/'):
+        run('tar -cvf %s media' % remote_media_dump)
+        run('gzip %s' % remote_media_dump)
+    
+    get('%s.gz' % remote_media_dump, '%s.gz' % local_media_dump)
+
+    local('rm -rf media')
+    local('mv %s.gz .' % local_media_dump)
+    local('tar -xzvf %s.gz' % media_filename)
+    local('rm %s.gz' % media_filename)
+
+
+@roles('staging')
+def push_staging_data():
+    filename = "%s-%s.sql" % (DB_NAME, uuid.uuid4())
+    local_path = "%s%s" % (LOCAL_DUMP_PATH, filename)
+    remote_path = "%s%s" % (REMOTE_DUMP_PATH, filename)
+    staging_db_backup_path = "%s%s-%s.sql" % (REMOTE_DUMP_PATH, DB_NAME, uuid.uuid4())
+
+    # dump and upload db
+    local('pg_dump -Upostgres -xOf %s %s' % (local_path, DB_NAME))
+    local('gzip %s' % local_path)
+    put("%s.gz" % local_path, "%s.gz" % remote_path)
+
+    run('pg_dump -xO -U%s -h %s -f %s' % (STAGING_DB_USERNAME, STAGING_DB_SERVER, staging_db_backup_path))
+    puts('Previous staging database backed up to %s' % staging_db_backup_path)
+    
+    run('gunzip %s.gz' % remote_path)
+    run('psql -U%s -h %s -c "DROP SCHEMA public CASCADE"' % (STAGING_DB_USERNAME, STAGING_DB_SERVER))
+    run('psql -U%s -h %s -c "CREATE SCHEMA public"' % (STAGING_DB_USERNAME, STAGING_DB_SERVER))
+    run('psql -U%s -h %s -f %s' % (STAGING_DB_USERNAME, STAGING_DB_SERVER, remote_path))
+    run('rm %s' % remote_path)
