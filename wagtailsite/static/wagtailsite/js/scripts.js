d1457cc2767e939c860ff1c052a03c1b4fbf4e1b
Balazs <balazs@torchbox.com>
2014-04-30 10:10:53 +0100

    General cleanup

--- wagtailsite/static/wagtailsite/js/scripts.js
+++ wagtailsite/static/wagtailsite/js/scripts.js
@@ -1,8 +1,15 @@
 
 $( document ).ready(function() {
+  $('.youtube').colorbox({iframe: true, width: 1280, height: 720, href:function(){
+    var videoId = new RegExp('[\\?&]v=([^&#]*)').exec(this.href);
+    if (videoId && videoId[1]) {
+        return 'http://youtube.com/embed/'+videoId[1]+'?rel=0&wmode=transparent';
+    }
+  }});
+
   $(".post-header").click(function () {
 
-    
+
       if ($(this).next().css('display') == 'none') {
           $(".post-body").css('display', 'none');
           $(this).next().css('display', 'block');
@@ -12,26 +19,26 @@ $( document ).ready(function() {
           $(this).next().css('display', 'none');
           $(this).removeClass('post-header__active');
       }
-      
-      
+
+
 //      if ($(this).hasClass('blurb-big')) {
 //        $(".blurb").removeClass('blurb-big');
 //      } else {
 //        $(".blurb").removeClass('blurb-big');
-//	   $(this).addClass('blurb-big');
+//      $(this).addClass('blurb-big');
 //      }
-      
+
 //      $(this).next().css('display','block');
-      
+
       /* if ($(this).next().css('display') == 'none') {
           $(".blogpost-content").css('display', 'none');
           $(this).next().css('display', 'block');
       } else {
           $(this).next().css('display', 'none');
       } */
-      
+
     });
-    
+
      $("#hamburger").click(function () {
     if ($('#mobilenav').css('display') == 'none') {
           $("#mobilenav").css('display', 'block');
@@ -39,7 +46,7 @@ $( document ).ready(function() {
           $('#mobilenav').css('display', 'none');
       }
     });
-    
+
 //    $("#hamburger").click(function () {
 //    if ($('#mobilenav').css('margin-top') == '-200px') {
 //          $("#mobilenav").css('margin-top', '0');
@@ -47,5 +54,5 @@ $( document ).ready(function() {
 //          $('#mobilenav').css('margin-top', '-200px');
 //      }
 //    });
-    
+
 });
